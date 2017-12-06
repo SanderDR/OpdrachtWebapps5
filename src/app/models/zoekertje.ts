8e3f6f5bc312aa13a693a1209b23d6af004750bf
Sander <Sander@SCHOOL>
2017-12-06 21:53:19 +0100

    kleine fout nog opgelost

--- src/app/models/zoekertje.ts
+++ src/app/models/zoekertje.ts
@@ -3,13 +3,13 @@ import {Reactie} from './reactie';
 
 export class Zoekertje {
     public _zoekertjeId: string;
-    public _name: string;
-    public _description: string;
-    public _price: Number;
-    public _location: string;
-    public _from: User;
-    public _pic: string;
-    public _comments: [Reactie];
+    public name: string;
+    public description: string;
+    public price: Number;
+    public location: string;
+    public from: User;
+    public pic: string;
+    public comments: [Reactie];
 
     static fromJSON(json): Zoekertje {
         const rec = new Zoekertje(json.name, json.description, json.price, json.location, json.from, json.pic, json.comments);
@@ -18,24 +18,24 @@ export class Zoekertje {
     }
 
     constructor(name: string, description: string, price: Number, location: string, from: User, pic: string, comments:[Reactie]) {
-        this._name = name;
-        this._description = description;
-        this._price = price;
-        this._location = location;
-        this._from = from;
-        this._pic = pic;
-        this._comments = comments;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.location = location;
+        this.from = from;
+        this.pic = pic;
+        this.comments = comments;
     }
 
     toJSON() {
         return {
-            name: this._name,
-            description: this._description,
-            price: this._price,
-            location: this._location,
-            from: this._from,
-            pic: this._pic,
-            comments: this._comments
+            name: this.name,
+            description: this.description,
+            price: this.price,
+            location: this.location,
+            from: this.from,
+            pic: this.pic,
+            comments: this.comments
         };
     }
 
